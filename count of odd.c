94562e874c75dd2cea9543739cac1ce6674ab417
ASathvikReddy <97426173+ASathvikReddy@users.noreply.github.com>
2022-01-21 08:50:37 +0530

    Add files via upload

---
+++ count of odd.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int n,count=0;
+	scanf("%d",&n);
+	int a[n],i;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		if(a[i]%2!=0)
+		{
+			count++;
+		}
+	}
+	printf("%d",count);
+}
